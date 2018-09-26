134b034baa5a5db79ce184dcba5ed26889dbaa6d
maik <mab603@gmail.com>
2018-09-26 13:46:11 +0200

    Initial commit

---
+++ Assets/_GameAssets/Scripts/Tuberias.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tuberias : MonoBehaviour {
+
+    [SerializeField] int speed = 3;
+
+	// Use this for initialization
+	void Start ()
+    {
+        float factorPosicion = Random.RandomRange(-4, 4);
+        transform.position = new Vector3
+            (
+            transform.position.x,
+            transform.position.y + factorPosicion,
+            transform.position.z
+            );
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GameConfig.IsPlaying())
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+            if (transform.position.x < -8)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
